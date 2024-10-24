d88c8ce98425167dbedd95bf82efd752b203d93c
곽윤호 <kwakdm95@gmail.com>
2024-10-24 15:19:00 +0900

    import objects...

--- src/main/java/me/kwakyunho/springbootdeveloper/TestController.java
+++ src/main/java/me/kwakyunho/springbootdeveloper/TestController.java
@@ -1,7 +1,10 @@
 package me.kwakyunho.springbootdeveloper;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class TestController {
--- src/main/java/me/kwakyunho/springbootdeveloper/TestService.java
+++ src/main/java/me/kwakyunho/springbootdeveloper/TestService.java
@@ -1,6 +1,9 @@
 package me.kwakyunho.springbootdeveloper;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class TestService {
