d9c69f09094cc6d2ff6d68d6130361c189b8dedf
곽윤호 <kwakdm95@gmail.com>
2024-10-24 17:06:43 +0900

    양수, 0, 음수, 값 크기 비교 테스트

--- src/test/java/JUnitQuiz.java
+++ src/test/java/JUnitQuiz.java
@@ -1,4 +1,3 @@
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -10,18 +9,24 @@ public class JUnitQuiz {
         String name2 = "홍길동";
         String name3 = "홍길은";
 
-//        Assertions.assertNotNull(name1);
-//        Assertions.assertNotNull(name2);
-//        Assertions.assertNotNull(name3);
-//
-//        Assertions.assertEquals(name2, name1);
-//        Assertions.assertNotEquals(name2, name3);
-
         assertThat(name1).isNotNull();
         assertThat(name2).isNotNull();
         assertThat(name3).isNotNull();
 
         assertThat(name1).isEqualTo(name2);
         assertThat(name2).isNotEqualTo(name3);
+    }
+
+    @Test
+    public void junitTest2() {
+        int number1 = 15;
+        int number2 = 0;
+        int number3 = -5;
+
+        assertThat(number1).isPositive();
+        assertThat(number2).isZero();
+        assertThat(number3).isNegative();
+        assertThat(number1).isGreaterThan(number2);
+        assertThat(number3).isLessThan(number2);
     }
 }
