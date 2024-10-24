12a8c81e640f26739fa541a0881908940d593943
곽윤호 <kwakdm95@gmail.com>
2024-10-24 12:18:03 +0900

    앱 실행 시점 작성, 컨트롤러 작성 애플리케이션 진입점 작성(SpringBootDeveloperApplication.java) 컨트롤러 작성 (TestController.java)

---
+++ src/main/java/me/kwakyunho/springbootdeveloper/TestController.java
@@ -0,0 +1,12 @@
+package me.kwakyunho.springbootdeveloper;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping("/test")
+    public String test() {
+        return "Hello World! this method is GET.";}
+
+}
