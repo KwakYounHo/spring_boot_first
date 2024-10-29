02c94a53d1025ae3cf802ae522d77b930899f256
곽윤호 <kwakdm95@gmail.com>
2024-10-29 22:47:47 +0900

    아티클 목록 반환 응답 작성

--- src/main/java/me/kwakyunho/springbootdeveloper/controller/BlogApiController.java
+++ src/main/java/me/kwakyunho/springbootdeveloper/controller/BlogApiController.java
@@ -3,12 +3,16 @@ package me.kwakyunho.springbootdeveloper.controller;
 import lombok.RequiredArgsConstructor;
 import me.kwakyunho.springbootdeveloper.domain.Article;
 import me.kwakyunho.springbootdeveloper.dto.AddArticleRequest;
+import me.kwakyunho.springbootdeveloper.dto.ArticleResponse;
 import me.kwakyunho.springbootdeveloper.service.BlogService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController // HTTP Response Body에 객체 데이터를 JSON 형식으로 반환하는 컨트롤러
@@ -25,5 +29,16 @@ public class BlogApiController {
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .body(savedArticle);
+    }
+
+    @GetMapping("/api/articles")
+    public ResponseEntity<List<ArticleResponse>> findAllArticles() {
+        List<ArticleResponse> articles = blogService.findAll()
+                .stream()
+                .map(ArticleResponse::new)
+                .toList();
+
+        return ResponseEntity.ok()
+                .body(articles);
     }
 }
