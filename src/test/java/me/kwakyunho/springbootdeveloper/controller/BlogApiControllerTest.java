b31da25d12482155ce32a1badce7ec518bdbf824
곽윤호 <kwakdm95@gmail.com>
2024-10-29 22:53:41 +0900

    컨트롤러 테스트 코드 오탈자 수정

--- src/test/java/me/kwakyunho/springbootdeveloper/controller/BlogApiControllerTest.java
+++ src/test/java/me/kwakyunho/springbootdeveloper/controller/BlogApiControllerTest.java
@@ -24,7 +24,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-class BlogServiceTest {
+class BlogApiControllerTest {
     @Autowired
     protected MockMvc mockMvc;
 
