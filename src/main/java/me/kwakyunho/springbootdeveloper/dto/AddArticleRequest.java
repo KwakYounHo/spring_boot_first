3c166e5604fd4525c83e5619596a2690fd044aae
곽윤호 <kwakdm95@gmail.com>
2024-10-29 20:50:55 +0900

    Getter 어노테이션 적용 해당 어노테이션이 없었기에 리플렉션이 진행되지 않았음

--- src/main/java/me/kwakyunho/springbootdeveloper/dto/AddArticleRequest.java
+++ src/main/java/me/kwakyunho/springbootdeveloper/dto/AddArticleRequest.java
@@ -1,11 +1,13 @@
 package me.kwakyunho.springbootdeveloper.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import me.kwakyunho.springbootdeveloper.domain.Article;
 
 @NoArgsConstructor // 매개변수 없는 생성자 (기본 생성자) 추가
 @AllArgsConstructor // 모든 필드를 매개변수로 갖는 생성 추가
+@Getter
 public class AddArticleRequest {
 
     private String title;
