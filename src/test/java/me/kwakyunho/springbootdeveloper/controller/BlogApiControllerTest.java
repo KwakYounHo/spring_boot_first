65a692b83a58906259c79e027ac98d3a0a73c3c1
곽윤호 <kwakdm95@gmail.com>
2024-10-30 10:06:10 +0900

    BlogApiController테스트 코드 리팩토링

--- src/test/java/me/kwakyunho/springbootdeveloper/controller/BlogApiControllerTest.java
+++ src/test/java/me/kwakyunho/springbootdeveloper/controller/BlogApiControllerTest.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import me.kwakyunho.springbootdeveloper.domain.Article;
 import me.kwakyunho.springbootdeveloper.dto.AddArticleRequest;
 import me.kwakyunho.springbootdeveloper.repository.BlogRepository;
+import me.kwakyunho.springbootdeveloper.service.BlogService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -38,6 +39,9 @@ class BlogApiControllerTest {
     @Autowired
     private ObjectMapper objectMapper;
 
+    @Autowired
+    private BlogService blogService;
+
     @BeforeEach
     public void setUp() {
         this.mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
@@ -46,28 +50,9 @@ class BlogApiControllerTest {
 
     private final String url = "/api/articles";
 
-    @DisplayName("POST(/api/articles) : addArticle() -> JSON 문자열을 요청 본문에 전달, 201, 본문 반환")
-    @Test
-    public void addArticleTest1() throws Exception {
-        // given
-        String requestBody = "{\"title\":\"기사 제목\", \"content\":\"기사 내용\"}";
-
-        // when
-        ResultActions result = mockMvc.perform(
-                post(url)
-                        .contentType(MediaType.APPLICATION_JSON_VALUE)
-                        .content(requestBody)
-        );
-
-        // then
-        result
-                .andExpect(status().isCreated())
-                .andExpect(jsonPath("$.title").value("기사 제목"));
-    }
-
     @DisplayName("POST(/api/articles) : addArticle() -> 객체를 직렬화 하여 요청, 아티클 레포지토리의 길이 및 0번째 데이터가 일치하는지 확인")
     @Test
-    public void addArticleTest2() throws Exception {
+    public void addArticleTest() throws Exception {
         // given
         String title = "title";
         String content = "content";
@@ -87,6 +72,7 @@ class BlogApiControllerTest {
         List<Article> articles = blogRepository.findAll();
 
         assertThat(articles.size()).isEqualTo(1);
+        assertThat(articles.get(0).getTitle()).isEqualTo(title);
         assertThat(articles.get(0).getContent()).isEqualTo(content);
     }
 
@@ -98,15 +84,11 @@ class BlogApiControllerTest {
         AddArticleRequest request2 = new AddArticleRequest("제목2", "내용2");
         AddArticleRequest request3 = new AddArticleRequest("제목3", "내용3");
 
-        String requestBody1 = objectMapper.writeValueAsString(request1);
-        String requestBody2 = objectMapper.writeValueAsString(request2);
-        String requestBody3 = objectMapper.writeValueAsString(request3);
+        blogService.save(request1);
+        blogService.save(request2);
+        blogService.save(request3);
 
         // when
-        mockMvc.perform(post(url).contentType(MediaType.APPLICATION_JSON_VALUE).content(requestBody1));
-        mockMvc.perform(post(url).contentType(MediaType.APPLICATION_JSON_VALUE).content(requestBody2));
-        mockMvc.perform(post(url).contentType(MediaType.APPLICATION_JSON_VALUE).content(requestBody3));
-
         ResultActions result = mockMvc
                 .perform(get(url)
                         .accept(MediaType.APPLICATION_JSON));
